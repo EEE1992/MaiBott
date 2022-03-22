1a0dcdee7c94942713051bbe3faa4569cdd0dcbc
EEEEEEE <enze.zheng1992@gmail.com>
2022-03-22 21:23:25 +0100

    DRAFT: in progress

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 // Require the necessary discord.js classes
 const { Client, Intents } = require('discord.js');
 const score = require('./src/score')
+const fetch = require('node-fetch')
+const cv = require('./opencv')
 
-const token = process.env['bot-token']
+require('dotenv').config()
+
+const token = process.env.TOKEN
 
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS] });
-
-function logMapElements(value, key, map) {
-  console.log(`m[${key}] = ${value.url}`);
-}
-
 
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
@@ -26,7 +25,7 @@ client.on('messageCreate', message => {
       text = await score.transformImageToText(value.url)
       message.reply(text);
     });
-      } else {
+  } else {
     message.reply('You can only update 1 score each time.');
   }
 })
--- src/score.js
+++ src/score.js
@@ -2,11 +2,14 @@ const { createWorker } = require('tesseract.js');
 
 const worker = createWorker();
 
-async function transformImageToText(url) {
+async function transformImageToText(image) {
   await worker.load();
-  await worker.loadLanguage('eng');
-  await worker.initialize('eng');
-  const { data: { text } } = await worker.recognize(url);
+  await worker.loadLanguage('equ');
+  await worker.initialize('equ');
+  await worker.setParameters({
+    tessedit_char_whitelist: '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ.%',
+  });
+  const { data: { text } } = await worker.recognize(image);
   return text;
 }
 
